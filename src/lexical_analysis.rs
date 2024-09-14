290416cbaf75d289037a9f045f27f3c3576170cc
tonyjaimep <tonyjaimep@gmail.com>
2024-09-14 10:11:35 -0600

    Lexical analysis ✅

--- src/lexical_analysis.rs
+++ src/lexical_analysis.rs
@@ -1,27 +1,244 @@
 use crate::token::*;
 
-fn build_token(input: char) -> Result<Token, String> {
-    let token_type = match input {
-        '0'..='9' => TokenType::Constant,
-        '+' | '-' | '*' | '/' => TokenType::Operator,
-        _ => return Err(format!("Unexpected {input}")),
-    };
+#[derive(Debug)]
+enum TokenBuildingStateType {
+    // empty stack
+    Empty,
+    // only single = in stack
+    Equal,
+    // only composable operators in stack
+    ComposableOperator,
+    // only a-zA-Z in stack
+    Alphabetic,
+    // only 0-9 in stack
+    Numeric,
+    // 0-9+. in stack
+    NumericPeriod,
+    // 0-9+.0-9+ in stack
+    NumericFloatingPoint,
+}
 
-    let result_token = Token {
-        value: input,
-        token_type,
-    };
+struct TokenBuildingState {
+    pub state_type: TokenBuildingStateType,
+    pub accumulator: String,
+    pub token_vector: Vec<Token>,
+}
 
-    Ok(result_token)
+fn push_token(state: TokenBuildingState, token: Token) -> TokenBuildingState {
+    TokenBuildingState {
+        state_type: TokenBuildingStateType::Empty,
+        accumulator: String::new(),
+        token_vector: vec![&state.token_vector[..], &[token]].concat(),
+    }
+}
+
+fn commit_accumulator(state: TokenBuildingState) -> Result<TokenBuildingState, String> {
+    let accumulator = state.accumulator.clone();
+    if accumulator.is_empty() {
+        Ok(state)
+    } else {
+        let token = Token::try_from(accumulator)?;
+
+        Ok(push_token(state, token))
+    }
+}
+
+fn accumulate_character(
+    character: char,
+    state: TokenBuildingState,
+    state_type: TokenBuildingStateType,
+) -> TokenBuildingState {
+    let mut accumulator = state.accumulator.clone();
+    accumulator.push(character);
+
+    TokenBuildingState {
+        state_type,
+        accumulator,
+        token_vector: state.token_vector,
+    }
+}
+
+fn unexpected_character_error(
+    character: char,
+    state: TokenBuildingState,
+) -> Result<TokenBuildingState, String> {
+    Err(format!(
+        "Unexpected {} after {}, with state {}",
+        if character.is_whitespace() {
+            String::from("whitespace")
+        } else {
+            character.to_string()
+        },
+        if state.accumulator.is_empty() {
+            String::from("empty string")
+        } else {
+            state.accumulator.to_string()
+        },
+        format!("{:?}", state.state_type)
+    ))
+}
+
+fn commit_accumulator_and_begin_with_character(
+    character: char,
+    state: TokenBuildingState,
+    state_type: TokenBuildingStateType,
+) -> Result<TokenBuildingState, String> {
+    let state_after_committing_accumulator = commit_accumulator(state)?;
+    return Ok(accumulate_character(
+        character,
+        state_after_committing_accumulator,
+        state_type,
+    ));
+}
+
+fn accumulate_character_and_commit_accumulator(
+    character: char,
+    state: TokenBuildingState,
+) -> Result<TokenBuildingState, String> {
+    // setting state to empty because it will not matter after accumulator is committed
+    commit_accumulator(accumulate_character(
+        character,
+        state,
+        TokenBuildingStateType::Empty,
+    ))
+}
+
+fn commit_accumulator_and_single_character_token(
+    character: char,
+    state: TokenBuildingState,
+) -> Result<TokenBuildingState, String> {
+    let state_after_committing_accumulator = commit_accumulator(state)?;
+    Ok(push_token(
+        state_after_committing_accumulator,
+        Token::try_from(character.to_string())?,
+    ))
+}
+
+fn handle_character(
+    character: char,
+    state: TokenBuildingState,
+) -> Result<TokenBuildingState, String> {
+    match character {
+        '{' | '}' | '(' | ')' | ';' => match state.state_type {
+            TokenBuildingStateType::NumericPeriod => unexpected_character_error(character, state),
+            _ => commit_accumulator_and_single_character_token(character, state),
+        },
+        '=' => match state.state_type {
+            TokenBuildingStateType::Numeric
+            | TokenBuildingStateType::Alphabetic
+            | TokenBuildingStateType::NumericFloatingPoint => {
+                commit_accumulator_and_begin_with_character(
+                    character,
+                    state,
+                    TokenBuildingStateType::Equal,
+                )
+            }
+            TokenBuildingStateType::ComposableOperator | TokenBuildingStateType::Equal => {
+                accumulate_character_and_commit_accumulator(character, state)
+            }
+            TokenBuildingStateType::Empty => Ok(accumulate_character(
+                character,
+                state,
+                TokenBuildingStateType::Equal,
+            )),
+            _ => unexpected_character_error(character, state),
+        },
+        '<' | '>' | '+' | '-' => match state.state_type {
+            TokenBuildingStateType::NumericPeriod => unexpected_character_error(character, state),
+            TokenBuildingStateType::ComposableOperator => {
+                accumulate_character_and_commit_accumulator(character, state)
+            }
+            TokenBuildingStateType::Empty => Ok(accumulate_character(
+                character,
+                state,
+                TokenBuildingStateType::ComposableOperator,
+            )),
+            TokenBuildingStateType::Alphabetic
+            | TokenBuildingStateType::NumericFloatingPoint
+            | TokenBuildingStateType::Numeric => commit_accumulator_and_begin_with_character(
+                character,
+                state,
+                TokenBuildingStateType::ComposableOperator,
+            ),
+            _ => unexpected_character_error(character, state),
+        },
+        '.' => match state.state_type {
+            TokenBuildingStateType::Numeric => Ok(accumulate_character(
+                character,
+                state,
+                TokenBuildingStateType::NumericPeriod,
+            )),
+            _ => unexpected_character_error(character, state),
+        },
+        _ if character.is_alphabetic() => match state.state_type {
+            TokenBuildingStateType::Empty | TokenBuildingStateType::Alphabetic => Ok(
+                accumulate_character(character, state, TokenBuildingStateType::Alphabetic),
+            ),
+            TokenBuildingStateType::NumericFloatingPoint
+            | TokenBuildingStateType::NumericPeriod
+            | TokenBuildingStateType::Numeric => unexpected_character_error(character, state),
+            _ => commit_accumulator_and_begin_with_character(
+                character,
+                state,
+                TokenBuildingStateType::Alphabetic,
+            ),
+        },
+        _ if character.is_whitespace() => {
+            return match state.state_type {
+                TokenBuildingStateType::NumericPeriod => {
+                    unexpected_character_error(character, state)
+                }
+                _ => commit_accumulator(state),
+            };
+        }
+        _ if character.is_numeric() => match state.state_type {
+            TokenBuildingStateType::NumericFloatingPoint
+            | TokenBuildingStateType::NumericPeriod => Ok(accumulate_character(
+                character,
+                state,
+                TokenBuildingStateType::NumericFloatingPoint,
+            )),
+            TokenBuildingStateType::Numeric | TokenBuildingStateType::Empty => Ok(
+                accumulate_character(character, state, TokenBuildingStateType::Numeric),
+            ),
+            TokenBuildingStateType::Alphabetic => unexpected_character_error(character, state),
+            _ => commit_accumulator_and_begin_with_character(
+                character,
+                state,
+                TokenBuildingStateType::Numeric,
+            ),
+        },
+        _ => unexpected_character_error(character, state),
+    }
+}
+
+fn build_tokens(
+    mut input: impl Iterator<Item = char>,
+    state: TokenBuildingState,
+) -> Result<TokenBuildingState, String> {
+    match input.next() {
+        Some(character) => build_tokens(input, handle_character(character, state)?),
+        None => Ok(push_token(
+            commit_accumulator(state)?,
+            Token {
+                value: None,
+                token_type: TokenType::EOF,
+            },
+        )),
+    }
 }
 
 pub fn lexical_analysis(
     input: impl Iterator<Item = char>,
 ) -> Result<impl Iterator<Item = Token>, String> {
-    let analysis_result: Result<Vec<Token>, String> = input.map(build_token).collect();
+    let token_building_state = build_tokens(
+        input,
+        TokenBuildingState {
+            state_type: TokenBuildingStateType::Empty,
+            accumulator: String::new(),
+            token_vector: vec![],
+        },
+    );
 
-    match analysis_result {
-        Ok(collection) => Ok(collection.into_iter()),
-        Err(message) => Err(message),
-    }
+    Ok(token_building_state?.token_vector.into_iter())
 }
--- src/token.rs
+++ src/token.rs
@@ -1,9 +1,151 @@
+use regex::Regex;
+use std::fmt;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum TokenType {
+    If,
+    Semicolon,
+    For,
+    ParenthesisOpening,
+    ParenthesisClosing,
+    OperatorMultiplication,
+    OperatorDivision,
+    OperatorAddition,
+    OperatorSubtraction,
+    OperatorIncrement,
+    OperatorIncrementBy,
+    OperatorDecrement,
+    OperatorDecrementBy,
+    OperatorAssignment,
+    OperatorLessThan,
+    OperatorLessThanOrEqual,
+    OperatorGreaterThan,
+    OperatorGreaterThanOrEqual,
+    OperatorEqual,
     Constant,
-    Operator,
+    Identifier,
+    CurlyOpening,
+    CurlyClosing,
+    EOF,
 }
 
+impl std::fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TokenType::If => "IF",
+                TokenType::Semicolon => ";",
+                TokenType::For => "FOR",
+                TokenType::ParenthesisOpening => "(",
+                TokenType::ParenthesisClosing => ")",
+                TokenType::OperatorAssignment => "=",
+                TokenType::OperatorMultiplication => "*",
+                TokenType::OperatorDivision => "/",
+                TokenType::OperatorSubtraction => "-",
+                TokenType::OperatorAddition => "+",
+                TokenType::OperatorIncrement => "++",
+                TokenType::OperatorDecrement => "--",
+                TokenType::OperatorIncrementBy => "+=",
+                TokenType::OperatorDecrementBy => "-=",
+                TokenType::OperatorLessThan => "LT",
+                TokenType::OperatorLessThanOrEqual => "LTE",
+                TokenType::OperatorGreaterThan => "GT",
+                TokenType::OperatorGreaterThanOrEqual => "GTE",
+                TokenType::OperatorEqual => "EQ",
+                TokenType::CurlyOpening => "{",
+                TokenType::CurlyClosing => "}",
+                TokenType::Constant => "Constant",
+                TokenType::Identifier => "Identifier",
+                TokenType::EOF => "EOF",
+            }
+        )
+    }
+}
+
+#[derive(Clone)]
 pub struct Token {
     pub token_type: TokenType,
-    pub value: char,
+    pub value: Option<u16>,
+}
+
+impl Token {
+    pub fn is_binary_operator(self: &Self) -> bool {
+        match self.token_type {
+            TokenType::OperatorMultiplication
+            | TokenType::OperatorDivision
+            | TokenType::OperatorAddition
+            | TokenType::OperatorSubtraction
+            | TokenType::OperatorIncrementBy
+            | TokenType::OperatorDecrementBy
+            | TokenType::OperatorAssignment
+            | TokenType::OperatorLessThan
+            | TokenType::OperatorLessThanOrEqual
+            | TokenType::OperatorGreaterThan
+            | TokenType::OperatorGreaterThanOrEqual
+            | TokenType::OperatorEqual => true,
+            TokenType::OperatorIncrement | TokenType::OperatorDecrement => false,
+            _ => panic!("Token is not operator: {}", self),
+        }
+    }
+}
+
+impl TryFrom<String> for Token {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let token_type = match value.as_str() {
+            "for" => TokenType::For,
+            "if" => TokenType::If,
+            ";" => TokenType::Semicolon,
+            "(" => TokenType::ParenthesisOpening,
+            ")" => TokenType::ParenthesisClosing,
+            "*" => TokenType::OperatorMultiplication,
+            "/" => TokenType::OperatorDivision,
+            "-" => TokenType::OperatorSubtraction,
+            "--" => TokenType::OperatorDecrement,
+            "-=" => TokenType::OperatorDecrementBy,
+            "++" => TokenType::OperatorIncrement,
+            "+=" => TokenType::OperatorIncrementBy,
+            "+" => TokenType::OperatorAddition,
+            ">" => TokenType::OperatorGreaterThan,
+            "<" => TokenType::OperatorLessThan,
+            ">=" => TokenType::OperatorGreaterThanOrEqual,
+            "<=" => TokenType::OperatorLessThanOrEqual,
+            "==" => TokenType::OperatorEqual,
+            "=" => TokenType::OperatorAssignment,
+            "{" => TokenType::CurlyOpening,
+            "}" => TokenType::CurlyClosing,
+            _ => {
+                if Regex::new(r"^[0-9]+$").unwrap().is_match(&value) {
+                    TokenType::Constant
+                } else if Regex::new(r"^[a-zA-Z]+$").unwrap().is_match(&value) {
+                    TokenType::Identifier
+                } else {
+                    return Err(format!("Invalid token '{value}'"));
+                }
+            }
+        };
+
+        let token_value = match token_type {
+            TokenType::Constant => Some(value.parse::<u16>().unwrap()),
+            _ => None,
+        };
+
+        Ok(Token {
+            token_type,
+            value: token_value,
+        })
+    }
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.value.is_some() {
+            write!(f, "{}<{}>", self.token_type, self.value.unwrap())
+        } else {
+            write!(f, "<{}>", self.token_type)
+        }
+    }
 }
